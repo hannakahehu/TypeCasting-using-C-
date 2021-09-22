1468f2b021b8ebec8133eb14d9ed559ed93c58ea
Hanna kahehu <39971210+hannakahehu@users.noreply.github.com>
2021-09-22 22:27:59 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   
+Console.WriteLine("Enter your age:");//  for output
+
+      int age = Convert.ToInt32(Console.ReadLine()); // our integera is converted to string this get user input  
+      Console.WriteLine("Your age is: " + age);//  Display output 
+  }
+}
